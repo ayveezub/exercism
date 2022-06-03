59494cf90bc78c2c04bb40ac8da402983c1a87c4
ayveezub <ayveezub@gmail.com>
2022-06-03 14:30:16 +0300

    js: refactor Collatz Conjecture and Triangle exercises

--- js/triangle.js
+++ js/triangle.js
@@ -4,11 +4,15 @@ Determine if a triangle is equilateral, isosceles, or scalene.
 
 An equilateral triangle has all three sides the same length.
 
-An isosceles triangle has at least two sides the same length. (It is sometimes specified as having exactly two sides the same length, but for the purposes of this exercise we'll say at least two.)
+An isosceles triangle has at least two sides the same length.
+(It is sometimes specified as having exactly two sides the same length,
+but for the purposes of this exercise we'll say at least two.)
 
 A scalene triangle has all sides of different lengths.
 
-Note: For a shape to be a triangle at all, all sides have to be of length > 0, and the sum of the lengths of any two sides must be greater than or equal to the length of the third side. See Triangle Inequality.
+Note: For a shape to be a triangle at all, all sides have to be of length > 0,
+and the sum of the lengths of any two sides must be greater than or equal to the length of the third side.
+(See Triangle Inequality.)
 
 */
 
@@ -23,23 +27,19 @@ export class Triangle {
     const otherTwoSides = this.sides.filter((elem, ind, arr) => ind !== arr.indexOf(maxSide))
     
     if (this.uniqueSides.has(0)) return false
-    if (maxSide <= otherTwoSides[0] + otherTwoSides[1]) return true
-    return false
+    return maxSide <= otherTwoSides[0] + otherTwoSides[1]
   }
 
   get isEquilateral() {
-    if (this.uniqueSides.size === 1 && this.isTriangle) return true
-    return false
+    return this.uniqueSides.size === 1 && this.isTriangle
   }
 
   get isIsosceles() {
     if (this.isEquilateral) return true
-    if (this.uniqueSides.size === 2 && this.isTriangle) return true
-    return false
+    return this.uniqueSides.size === 2 && this.isTriangle
   }
 
   get isScalene() {
-    if (this.uniqueSides.size === 3 && this.isTriangle) return true
-    return false
+    return this.uniqueSides.size === 3 && this.isTriangle
   }
 }
